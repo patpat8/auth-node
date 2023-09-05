04d67386c3b439b9d84aafcc2f1e99a291cb839a
Prathmesh Tokekar <patpat8@users.noreply.github.com>
2023-09-05 09:02:12 +0530

    added another origin as localhost

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL, 'http://localhost:3001'],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
