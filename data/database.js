12eefb18c531b7901c6934c96133b6db001d6c20
Prathmesh Tokekar <patpat8@users.noreply.github.com>
2023-09-05 11:03:39 +0530

    Jai Shree Ram

--- data/database.js
+++ data/database.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
-      dbName: "nodeapi",
+    .connect("mongodb://prompts-db:SGuaJ3nNCoIvqkfOA0gifDOgpvCAhDzTW5cpdDrWKvhIPEA0YOwgcUWDNUDPtx1aDPEBS8DOe8nfACDbYM554g%3D%3D@prompts-db.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@prompts-db@", {
+      dbName: "SalkDB",
     })
     .then((c) => {
       console.log(`Connected to ${c.connection.host}`);
